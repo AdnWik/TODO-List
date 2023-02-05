772967ac3da9c16aed0f4bc09b2a357f784946c7
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-02-05 18:14:04 +0100

    Create database and add test data

--- main.py
+++ main.py
@@ -1,2 +1,65 @@
-class Todo:
-    pass
+import os
+import datetime
+from peewee import *
+
+
+dataBaseName = 'todo'+'.db'
+
+# TODO:
+if os.path.exists(dataBaseName):
+    os.remove(dataBaseName)
+
+if not os.path.exists(dataBaseName):
+    db = SqliteDatabase(dataBaseName)  # ':memory:'
+
+
+class BaseModel(Model):
+    class Meta:
+        database = db
+
+
+class TaskState(BaseModel):
+    state = CharField(null=False)
+
+
+class Task(BaseModel):
+    task = CharField(null=False)
+    createDate = DateTimeField(default=datetime.datetime.now)
+    deadLine = DateTimeField()
+    state = ForeignKeyField(TaskState, null=False, default=1)
+
+
+def initialization_data():
+    db.connect()
+    db.create_tables([Task, TaskState])
+
+    todo = TaskState(state='To Do')
+    todo.save()
+
+    inp = TaskState(state='In progress')
+    inp.save()
+
+    done = TaskState(state='Done')
+    done.save()
+
+    db.close()
+
+
+initialization_data()
+
+
+while True:
+
+    print('\nThis is test message')
+    print('\n0 - END\n1- Add task\n')
+    choice = int(input('\n-> '))
+
+    if choice == 0:
+        break
+
+    if choice == 1:
+        print('Processing ......')
+        db.connect()
+        task1 = Task(task='test', deadLine="2023-02-07", state=1)
+        task1.save()
+        db.close()
