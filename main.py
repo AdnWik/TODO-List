dd22f08db9c31dfb0d538de77423875ac0ddaf53
Adrian Wójcik <wojcik.adrian@protonmail.com>
2023-02-06 20:31:48 +0100

    Add show all task feature

--- main.py
+++ main.py
@@ -6,11 +6,12 @@ from peewee import *
 dataBaseName = 'todo'+'.db'
 
 # TODO:
+"""
 if os.path.exists(dataBaseName):
     os.remove(dataBaseName)
-
-if not os.path.exists(dataBaseName):
-    db = SqliteDatabase(dataBaseName)  # ':memory:'
+"""
+# if not os.path.exists(dataBaseName):
+db = SqliteDatabase(dataBaseName)  # ':memory:'
 
 
 class BaseModel(Model):
@@ -51,15 +52,25 @@ initialization_data()
 while True:
 
     print('\nThis is test message')
-    print('\n0 - END\n1- Add task\n')
+    print('\n0 - END\n1- Add task\n2 - Show all tasks\n')
     choice = int(input('\n-> '))
 
     if choice == 0:
         break
 
     if choice == 1:
-        print('Processing ......')
+        print('Create new task')
+        taskName = input('Eneter task name -> ')
+        deadLine = input('Enter deadline (YYYY-MM-DD) -> ')
         db.connect()
-        task1 = Task(task='test', deadLine="2023-02-07", state=1)
+        task1 = Task(task=taskName, deadLine=deadLine, state=1)
         task1.save()
+        db.close()
+
+    if choice == 2:
+        db.connect()
+        print('\nAll task:\n')
+        for task in Task.select(Task, TaskState).join(TaskState):
+            print(
+                f"Id: {task.id:<5} Task: {task.task:<50} State: {task.state.state:<8}")
         db.close()
